dcc34681b985e234ef96793a6b9836a9ac1ecb00
Thomas Jenkins <Jenkinsth@yahoo.com>
2022-12-12 16:01:08 -0800

    fixed SQL queries to join Rooms and objects, added if statements for main room room to investigate objects in room.

--- sql/setup.sql
+++ sql/setup.sql
@@ -26,6 +26,7 @@ CREATE TABLE items (
 
 CREATE TABLE objects (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    rooms_id BIGINT,
     object_name VARCHAR,
     object_description VARCHAR,
     object_secret_one VARCHAR,
@@ -36,8 +37,7 @@ CREATE TABLE objects (
 CREATE TABLE rooms (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     room_name VARCHAR,
-    room_description VARCHAR,
-    room_objects VARCHAR
+    room_description VARCHAR
 );
 
 CREATE TABLE rooms_objects (
@@ -70,6 +70,7 @@ VALUES
 ('Journal', 'A worn leather bound journal.', 'The final page has been ripped out but the following can be read from top to bottom on the remaining scraps of page: Doo... Co... 7');
 
 INSERT INTO objects (
+    rooms_id,
     object_name,
     object_description,
     object_secret_one,
@@ -77,11 +78,11 @@ INSERT INTO objects (
     object_secret_three
 )
 VALUES 
-('Desk', 'A small wooden desk with a lit lantern and a journal on the surface.', 'A small wooden desk with a lit lantern on the surface', 'A small wooden desk with a leather bound journal on the surface.', 'A small wooden desk with nothing on it. Cause you took it all...'),
-('Bunk Beds', 'A set of three bunk beds. All neatly made. Searching the beds you find a Small Key under the pillow of the bottom bunk.', 'A set of three bunk beds. you messed all the blankets up though.', null, null),
-('A Small metal Lock Box', 'A small metal lockbox beaten and weathered. It is currently locked.', 'A small metal lockbox. Beaten and weathered. You unlock it with the key you found. inside you find a Weathered Piece of Paper. The paper has the numbers 5, 1, 3.', 'A small metal lockbox. Beaten and weathered. It sits open and empty.', null),
-('Window', 'A small glass window divided into 4 sections with wooden trim. It is too dark outside to see anything. There seems to be something written on the glass but you cant make it out in the dim light.', 'A small glass window divided into 4 sections with wooden trim. With the light of the lantern you can make out the numbers 4, 2, 6.', null, null),
-('Door', 'A wooden door locked tight from the inside. A combination lock is keepiong the latch closed.', 'The door hangs open revealing a dark path into the forrest.', null, null);
+(1,'Desk', 'A small wooden desk with a lit lantern and a journal on the surface.', 'A small wooden desk with a lit lantern on the surface', 'A small wooden desk with a leather bound journal on the surface.', 'A small wooden desk with nothing on it. Cause you took it all...'),
+(1, 'Bunk Beds', 'A set of three bunk beds. All neatly made. Searching the beds you find a Small Key under the pillow of the bottom bunk.', 'A set of three bunk beds. you messed all the blankets up though.', null, null),
+(1,'A Small metal Lock Box', 'A small metal lockbox beaten and weathered. It is currently locked.', 'A small metal lockbox. Beaten and weathered. You unlock it with the key you found. inside you find a Weathered Piece of Paper. The paper has the numbers 5, 1, 3.', 'A small metal lockbox. Beaten and weathered. It sits open and empty.', null),
+(1, 'Window', 'A small glass window divided into 4 sections with wooden trim. It is too dark outside to see anything. There seems to be something written on the glass but you cant make it out in the dim light.', 'A small glass window divided into 4 sections with wooden trim. With the light of the lantern you can make out the numbers 4, 2, 6.', null, null),
+(1, 'Door', 'A wooden door locked tight from the inside. A combination lock is keepiong the latch closed.', 'The door hangs open revealing a dark path into the forrest.', null, null);
 
 
 INSERT INTO rooms (
@@ -90,6 +91,18 @@ INSERT INTO rooms (
 )
 VALUES
 ('hunting cabin', 'The room is a sparsely furnished hunting cabin. With log walls and a plank floor. Behind you is a Bunk Bed with three beds all neatly made. To the right of you is a small Desk. A lantern sitting on the desk offers the only dim light in the room. To the left of you is a small lock box, beaten and worn from years of use. To the left of you is a small glass window sitting above the lockbox. In front of you is a closed wooden door.');
+
+
+INSERT INTO rooms_objects (
+    rooms_id,
+    objects_id
+)
+VALUES 
+(1, 1),
+(1, 2),
+(1, 3),
+(1, 4),
+(1, 5);
 
 -- SELECT
 -- 	rooms
